6abedd599b4bd84b7dda7cb7d2b9e8d6b5698b6c
Ian Travers <mr.itpe@gmail.com>
2022-04-11 13:33:23 +0300

    Configure Inertia

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1 class="font-semibold text-2xl">Hello World with Inertia</h1>
+</template>
+
+<script>
+export default {
+    name: "Welcome"
+}
+</script>
+
+<style scoped>
+
+</style>
