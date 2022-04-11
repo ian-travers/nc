7a55eeaa1d86d880997b5fd6bbe10cc611bd0d81
Ian Travers <mr.itpe@gmail.com>
2022-04-11 15:13:12 +0300

    First Inertia page with props

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1 class="font-semibold text-2xl">Hello World with Inertia and Hello, {{ name }}</h1>
+</template>
+
+<script setup>
+defineProps({
+    'name': {
+        type: String,
+        default: 'A N Owen'
+    }
+})
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return inertia('Welcome');
+    return Inertia::render('Home', [
+        'name' => 'Ian Travers',
+    ]);
 });
