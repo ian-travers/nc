e9e2c2b726ce4623da04f8d44874302b464f8de7
Ian Travers <mr.itpe@gmail.com>
2022-04-28 11:19:18 +0300

    Modal components

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -13,31 +13,59 @@
     </div>
 
     <div class="text-blue-300 max-w-7xl mx-auto mt-8">
-        <p class="mb-6">Element Plus Playground</p>
-        <ElButton
-            type="primary"
-            @click="show = !show"
-        >
-            DefElBut
-        </ElButton>
+        <div class="flex justify-between space-x-4">
+            <div class="w-full">
+                <p class="mb-6">Element Plus Playground</p>
+                <ElButton
+                    type="primary"
+                    @click="show = !show"
+                >
+                    DefElBut
+                </ElButton>
 
-        <ElDialog
-            v-model="show"
-            title="Title"
-        >
-            Dialog body goes here
-            <slot name="footer" class="flex block">
-                <ElButton>Cancel</ElButton>
-                <ElButton>Confirm</ElButton>
-            </slot>
-        </ElDialog>
+                <ElDialog
+                    v-model="show"
+                    title="Title"
+                >
+                    Dialog body goes here
+                    <slot name="footer" class="flex block">
+                        <ElButton>Cancel</ElButton>
+                        <ElButton>Confirm</ElButton>
+                    </slot>
+                </ElDialog>
+            </div>
+            <div class="w-full">
+                <p class="mb-8">Custom dialog</p>
+
+                <a href="#cancel-modal" class="text-white bg-gray-600 hover:bg-gray-500 px-4 py-2 rounded">
+                    Open custom modal
+                </a>
+
+
+            </div>
+        </div>
     </div>
+
+    <modal name="cancel-modal">
+        <h1 class="text-3xl font-bold mb-4">Leaving So Soon?</h1>
+
+        <p class="mb-8">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem eligendi expedita odio quasi qui! A ad aspernatur blanditiis dolorem ea, eum impedit ipsa ipsum laboriosam maiores maxime nihil nulla odit omnis quae ratione sapiente sequi sit totam veniam! Animi hic ipsum minus officiis quisquam.
+        </p>
+
+        <template v-slot:footer>
+            <a href="#" class="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded-md mr-2">Cancel</a>
+            <a href="#" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md">Confirm</a>
+        </template>
+    </modal>
 </template>
 
 <script setup>
 import { ElButton } from 'element-plus';
 import { ElDialog } from 'element-plus';
-import 'element-plus/dist/index.css'
+import 'element-plus/dist/index.css';
+
+import Modal from "../Shared/Modal";
 
 defineProps({
     'name': String,
---
+++ resources/js/Shared/Modal.vue
@@ -0,0 +1,63 @@
+<template>
+    <div
+        :id="name" class="overlay">
+        <a href="#" class="cancel"></a>
+        <div class="modal">
+            <slot></slot>
+            <footer class="flex">
+                <slot name="footer"></slot>
+            </footer>
+            <a href="#" class="close text-3xl">&times;</a>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({name: String});
+</script>
+
+<style scoped>
+.overlay {
+    visibility: hidden;
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: rgba(0, 0, 0, .4);
+    transition: opacity 1s;
+    opacity: 0;
+}
+
+.overlay:target {
+    visibility: visible;
+    opacity: 1;
+}
+
+.modal {
+    position: relative;
+    width: 600px;
+    max-width: 80%;
+    background: white;
+    border-radius: 4px;
+    padding: 1em 2.5em;
+    box-shadow: 0 5px 11px rgba(0, 128, 128, .4);
+}
+
+.modal .close {
+    position: absolute;
+    top: 15px;
+    right: 20px;
+    color: gray;
+    text-decoration: none;
+}
+
+.overlay .cancel {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+}
+</style>
