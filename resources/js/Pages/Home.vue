339d050385cd128f121c84f036ba265a495ff28b
Ian Travers <mr.itpe@gmail.com>
2022-04-28 10:24:40 +0300

    Install and try element-plus

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -11,8 +11,36 @@
     <div class="text-blue-300">
         <h1 class="font-semibold text-2xl">Hello World with Inertia and Hello, {{ name }}</h1>
     </div>
+
+    <div class="text-blue-300 max-w-7xl mx-auto mt-8">
+        <p class="mb-6">Element Plus Playground</p>
+        <ElButton
+            type="primary"
+            @click="show = !show"
+        >
+            DefElBut
+        </ElButton>
+
+        <ElDialog
+            v-model="show"
+            title="Title"
+        >
+            Dialog body goes here
+            <slot name="footer" class="flex block">
+                <ElButton>Cancel</ElButton>
+                <ElButton>Confirm</ElButton>
+            </slot>
+        </ElDialog>
+    </div>
 </template>
 
 <script setup>
-defineProps({'name': String})
+import { ElButton } from 'element-plus';
+import { ElDialog } from 'element-plus';
+import 'element-plus/dist/index.css'
+
+defineProps({
+    'name': String,
+    'show': Boolean
+})
 </script>
